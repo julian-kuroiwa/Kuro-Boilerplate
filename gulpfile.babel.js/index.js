b85d919524ef3b3142831fdff540a9daaa8cdf2f
Julian Kuroiwa <Julian.Kuroiwa@wunderman.com>
2017-12-21 16:14:19 -0200

    Added Image task

--- gulpfile.babel.js/index.js
+++ gulpfile.babel.js/index.js
@@ -5,6 +5,7 @@ import htmlmin from 'gulp-htmlmin';
 import browserSync from 'browser-sync';
 import plumber from 'gulp-plumber';
 import nunjucksRender from 'gulp-nunjucks-render';
+import imagemin from 'gulp-imagemin';
 import path from 'path';
 import del from 'del';
 import sass from 'gulp-sass';
@@ -23,32 +24,44 @@ const paths = {
     css: {
         src: path.join(CONFIG.root.src, CONFIG.tasks.css.src, '/**/*.{' + CONFIG.tasks.css.extensions + '}'),
         dest: path.join(CONFIG.root.dest, CONFIG.tasks.css.dest)
+    },
+    img: {
+        src: path.join(CONFIG.root.src, CONFIG.tasks.images.src, '/**'),
+        dest: path.join(CONFIG.root.dest, CONFIG.tasks.images.dest)
     }
 };
 
 gulp.task('html', () => {
     return gulp.src(paths.html.src)
         .pipe(plumber())
-        .pipe(nunjucksRender({
-            path: ['src/html']
-        }))
+        .pipe(nunjucksRender({ path: ['src/html'] }))
         .pipe(htmlmin({ collapseWhitespace: true, removeComments: true }))
         .pipe(gulp.dest(paths.html.dest))
         .pipe(browserSync.stream({ once: true }));
 });
 
+gulp.task('image', () => {
+    return gulp.src(paths.img.src)
+        .pipe(plumber())
+        .pipe(imagemin({
+            optimizationLevel: 3,
+            progressive: true,
+            interlaced: true
+        }))
+        .pipe(gulp.dest(paths.img.dest));
+});
+
 gulp.task('sass', () => {
     return gulp.src(paths.css.src)
+        .pipe(plumber())
         .pipe(sourcemaps.init())
-        .pipe(sass({
-            outputStyle: 'expanded'
-        }).on('error', sass.logError))
+        .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
         .pipe(autoprefixer())
         .pipe(cleanCSS())
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(paths.css.dest))
         .pipe(browserSync.stream());
-})
+});
 
 gulp.task('clean', () => {
     return del('build/');
@@ -63,13 +76,15 @@ gulp.task('browser-sync', () => {
 });
 
 gulp.task('watch', () => {
-    gulp.watch(paths.html.src, ['html']);
+    gulp.watch(paths.html.src, { debounceDelay: 300 }, ['html']);
     gulp.watch(paths.css.src, ['sass']);
+    gulp.watch(paths.img.src, ['image']);
 });
 
 gulp.task('build', ['clean'], () => {
     gulp.start('html');
     gulp.start('sass');
+    gulp.start('image');
 });
 
 gulp.task('default', ['watch', 'browser-sync']);
